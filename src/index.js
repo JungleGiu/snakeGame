5b3d6ec2d4aaacff148c4fd45f1dfd97c280ae7e
Giu Eminente <giulemi210@gmail.com>
2026-01-26 17:31:52 +0100

    Random food position is determined at every reacharge

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
-foodRandomPosition = () => {
-    return {
-        x: Math.floor(Math.random() * 20),
-        y: Math.floor(Math.random() * 20),
-    };
+"use strict";
+
+ const foodRandomPosition = () => {
+  return {
+    x: Math.floor(Math.random() * 20),
+    y: Math.floor(Math.random() * 20),
+  };
 };
+
+const viewFood = () => {
+  const foodPosition = foodRandomPosition();
+  document.querySelector("#food").style.gridRow = foodPosition.y;
+  document.querySelector("#food").style.gridColumn = foodPosition.x;
+};
+window.onload = viewFood;
+
+const snakeX = document.querySelector("#snake").style.gridColumn.value;
+const snakeY = document.querySelector("#snake").style.gridRow.value;
+
+const snakePosition = {
+  x: snakeX,
+  y: snakeY,
+};
+console.log(snakePosition);
+// window.addEventListener("keydown", (event) => {
+//     switch (event.key) {
+//         case "ArrowUp":
+//             direction = "up";
+//             break;
+//         case "ArrowDown":
+//             direction = "down";
+//             break;
+//         case "ArrowLeft":
+//             direction = "left";
+//             break;
+//         case "ArrowRight":
+//             direction = "right";
+//             break;
+//     }
+// });
